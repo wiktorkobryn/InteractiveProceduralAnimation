0f00c93d01b6ca3f38c658ee163ab531673e50e2
wiktorkobryn <152988289+wiktorkobryn@users.noreply.github.com>
2025-03-31 08:50:10 +0200

    added: walker controller keyboard inputs

--- Assets/Scripts/WalkerController.cs
+++ Assets/Scripts/WalkerController.cs
@@ -11,11 +11,24 @@ public class WalkerController : MonoBehaviour
     private RaycastHit hit;
     private Vector3 targetPosition;
 
+    [Header("Movement")]
+    public float movementSpeed = 20.0f;
+    public float rotationSpeed = 20.0f;
+    private float movementVerical, movementHorizontal;
+
+    private void Start()
+    {
+        walkerBody.localPosition = bodyOffset;
+    }
+
+    #region orientation_control
+
     private void SetPositionFromPlane()
     {
-        targetPosition = walkerBody.position;
+        targetPosition = this.transform.position;
         targetPosition.y = hit.point.y;
-        walkerBody.position = targetPosition + bodyOffset;
+
+        this.transform.position = targetPosition;
     }
 
     private void SetRotationFromPlane()
@@ -31,4 +44,31 @@ public class WalkerController : MonoBehaviour
             SetRotationFromPlane();
         }
     }
+
+    #endregion
+
+    #region movement_inputs
+    private void ApplyRotation()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            transform.Rotate(Vector3.down * rotationSpeed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.E))
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+
+    private void ApplyPosition()
+    {
+        movementVerical = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+        movementHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+
+        transform.Translate(new Vector3(movementHorizontal, 0, movementVerical));
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyRotation();
+        ApplyPosition();
+    }
+
+    #endregion
 }
