046532d9151db5be81de4d1e311d41f91ef7c89d
wiktorkobryn <152988289+wiktorkobryn@users.noreply.github.com>
2025-04-06 21:44:00 +0200

    fix: piston inspector & visual adjustments

--- Assets/Piston.cs
+++ Assets/Piston.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 
 public class Piston : MonoBehaviour
 {
-    private bool isMoving = false;
     public Transform arm;
-    public float pushDistance = 1.0f, moveDuration = 1.0f;
+    public float pushDistance = 1.0f;
+    [Range(0.01f, 2.0f)]
+    public float moveDuration = 1.0f;
+    public KeyCode pushKey = KeyCode.P;
+
+    private bool isMoving = false;
     private Vector3 armRestPose, armTargetPose;
     public Vector3 axisToPush = new Vector3(1, 0, 0);
 
@@ -28,7 +32,7 @@ public class Piston : MonoBehaviour
 
         while(true)
         {
-            if (isMoving == false && Input.GetKey(KeyCode.P))
+            if (isMoving == false && Input.GetKey(pushKey))
             {
                 pushing = StartCoroutine(Transf3D.MoveOverTimeLinear(arm, moveDuration, armRestPose, armTargetPose, false, false));
                 yield return pushing;
